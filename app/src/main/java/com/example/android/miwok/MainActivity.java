74709d80f69d04c415dbd8609bf3e8491418ae4f
sascha <saschaheid@gmail.com>
2017-05-01 21:23:04 +0200

    arraylist

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,12 +29,45 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-    }
 
-    public void oeffneZahlen (View view){
-        Intent i = new Intent(this, Zahlen.class);
-        startActivity(i);
-    }
+        TextView numbers = (TextView) findViewById(R.id.numbers);
 
-    NumbersClickListener clickListener = new NumbersClickListener();
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, Zahlen.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this, Farben.class);
+                startActivity(colorsIntent);
+            }
+        });
+
+        TextView family = (TextView) findViewById(R.id.family);
+
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this, Familienmitglieder.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent phrasesIntent = new Intent(MainActivity.this, Ausdruecke.class);
+                startActivity(phrasesIntent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/android/miwok/Zahlen.java
+++ app/src/main/java/com/example/android/miwok/Zahlen.java
@@ -1,7 +1,10 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class Zahlen extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class Zahlen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zahlen);
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eigth");
+        words.add("nine");
+        words.add("ten");
+
+        for(int i=0; i<10; i++){
+            Log.v("Zahlen.java", "" + words.get(i));
+        }
     }
 }
